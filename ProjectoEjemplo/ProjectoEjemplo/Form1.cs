6146b29b663c9617e1fe11c4d11a4c5208a0c9b7
Mario <andresito.asb@gmail.com>
2024-10-25 19:14:42 -0500

    se crea modelo y se conecta a interfaz grafica

--- ProjectoEjemplo/ProjectoEjemplo/Form1.cs
+++ ProjectoEjemplo/ProjectoEjemplo/Form1.cs
@@ -12,9 +12,16 @@ namespace ProjectoEjemplo
 {
     public partial class Form1 : Form
     {
+        private tienda tienda;
         public Form1()
         {
             InitializeComponent();
+            this.tienda = new tienda(1000000.0, new libro[20]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ ProjectoEjemplo/ProjectoEjemplo/model.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectoEjemplo
+{
+    public class tienda
+    {
+        private double caja;
+        private libro []catalogo;
+
+        public tienda(double caja, libro[] catalogo)
+        {
+            this.Caja = caja;
+            this.Catalogo = catalogo;
+            MessageBox.Show("se ha creado la tienda con " + caja + " en la caja");
+        }
+
+        public double Caja { get => caja; set => caja = value; }
+        public libro[] Catalogo { get => catalogo; set => catalogo = value; }
+    }
+   public class libro
+    {
+        private int iSBN;
+        private string titulo;
+        private int cantidad_actual;
+        private double precio_de_venta;
+        private double precio_de_compra;
+
+        public libro(int iSBN, string titulo, int cantidad_actual, double precio_de_venta, double precio_de_compra)
+        {
+            this.ISBN = iSBN;
+            this.Titulo = titulo;
+            this.Cantidad_actual = cantidad_actual;
+            this.Precio_de_venta = precio_de_venta;
+            this.Precio_de_compra = precio_de_compra;
+        }
+
+        public int ISBN { get => iSBN; set => iSBN = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public int Cantidad_actual { get => cantidad_actual; set => cantidad_actual = value; }
+        public double Precio_de_venta { get => precio_de_venta; set => precio_de_venta = value; }
+        public double Precio_de_compra { get => precio_de_compra; set => precio_de_compra = value; }
+    }
+    public class transaccion
+    {
+        private libro libro;
+        private DateTime date;
+        private string tipo;
+
+        public transaccion(libro libro, DateTime date, string tipo)
+        {
+            this.Libro = libro;
+            this.Date = date;
+            this.Tipo = tipo;
+        }
+
+        public libro Libro { get => libro; set => libro = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+    }
+}
